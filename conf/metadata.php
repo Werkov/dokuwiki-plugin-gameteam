ac1da968973da5ad0ffe344192be75f8803e86ad
Michal Koutný <xm.koutny@gmail.com>
2015-03-02 02:02:07 +0100

    Fixed configuration metadata

--- conf/metadata.php
+++ conf/metadata.php
@@ -6,6 +6,7 @@
  */
 
 
+$conf['account'] = array('string');
 $meta['mysql_host'] = array('string');
 $meta['mysql_user'] = array('string');
 $meta['mysql_password'] = array('password');
@@ -15,6 +16,7 @@ $meta['show_upload'] = array('onoff');
 $meta['superuser_login'] = array('string');
 $meta['superuser_password'] = array('string');
 $meta['teamfields'] = array('');
+$meta['upload_year'] = array('numeric');
 $meta['volume_id'] = array('numeric');
 $meta['vs_prefix'] = array('string');
 $meta['vs_length'] = array('numeric');
